b851067f7ba434a766b7474bd782d6228344aaa9
Harshita <chandaliyaharshita222@gmail.com>
2023-05-20 19:16:54 +0530

    Added Welcome Message

---
+++ Data_Structures_And_Algorithms/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Data_Structures_And_Algorithms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Data Structures and Algorithms Practice Problems!");
+        }
+    }
+}
