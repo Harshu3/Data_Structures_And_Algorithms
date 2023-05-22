40d6293832a47a265f3d70ee6a8ab8ddc6d889c3
Harshita <chandaliyaharshita222@gmail.com>
2023-05-22 22:38:42 +0530

    Bubble Sort Problem

---
+++ Data_Structures_And_Algorithms/BubbleSort.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data_Structures_And_Algorithms
+{
+    internal class BubbleSort
+    {
+        public void Sort(int[] arr)
+        {
+            int size = arr.Length;
+            for (int i = 0; i < size - 1; i++)
+                for (int j = 0; j < size - i - 1; j++)
+                    if (arr[j] > arr[j + 1])
+                    {
+                        int temp = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = temp;
+                    }
+        }
+        public void Display(int[] arr)
+        {
+            int n = arr.Length;
+            for (int i = 0; i < n; ++i)
+            {
+                Console.Write(arr[i] + " ");
+            }
+        }
+    }
+}
--- Data_Structures_And_Algorithms/Program.cs
+++ Data_Structures_And_Algorithms/Program.cs
@@ -4,40 +4,22 @@ namespace Data_Structures_And_Algorithms
 {
     class Program
     {
-        static void permute(string str, int sI, int eI)
-        {
-            if (sI == eI)
-                Console.WriteLine(str);
-            else
-            {
-                for (int i = sI; i <= eI; i++)
-                {
-                    str = swap(str, sI, i);
-                    permute(str, sI + 1, eI);
-                    str = swap(str, sI, i);
-                }
-            }
-        }
-
-        public static string swap(string a, int i, int j)
-        {
-            char temp;
-            char[] charArray = a.ToCharArray();
-            temp = charArray[i];
-            charArray[i] = charArray[j];
-            charArray[j] = temp;
-            string s = new string(charArray);
-            return s;
-        }
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Data Structures and Algorithms Practice Problems!");
-            Console.WriteLine("Program of Permutations of a String!");
-            Console.WriteLine("\nEnter any no of alphabets:");
-            string str = Console.ReadLine();
-            Console.WriteLine("All possible permutations of a string: {0}", str);
-            int n = str.Length;
-            permute(str, 0, n - 1);
+            Console.WriteLine("Program of Bubble Sort to sort the array!");
+            int[] arr;
+            Console.WriteLine("Enter array size:");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter {0} elements: ", n);
+            arr = new int[n];
+            for (int i = 0;i < n; ++i)
+            {
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            BubbleSort s = new BubbleSort();
+            s.Sort(arr);
+            s.Display(arr);
         }
     }
 }
